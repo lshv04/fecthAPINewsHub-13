fd54e1976f7ce791ae2503ba2310796f658c6a64
Leandro Hosken <lshv04@gmail.com>
2024-08-27 15:55:44 -0300

    acrescentando api dinamica e multiplos componentes

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const apiKey = 'c6d4a244596e4be0a40ccfdce20659e1';
-const pageSize = 20; // Número máximo de artigos a retornar
-const category = 'business'; // Categoria de notícias
+const pageSize = 5; // Número máximo de artigos a retornar
 
-const url = `https://newsapi.org/v2/top-headlines?country=br&category=${category}&pageSize=${pageSize}&apiKey=${apiKey}`;
+// Função para fazer a requisição para uma categoria específica
+function fetchNewsByCategory(category) {
+  const url = `https://newsapi.org/v2/top-headlines?country=br&category=${category}&pageSize=${pageSize}&apiKey=${apiKey}`;
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    const articles = data.articles;
-    const container = document.querySelector("#newsContainer"); // Certifique-se de ter um contêiner no seu HTML
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const articles = data.articles;
+      const container = document.querySelector(`#${category}Container`);
 
-    // Função para criar HTML para uma notícia
-    function createArticleHTML(article) {
-      const date = new Date(article.publishedAt).toLocaleDateString('pt-BR'); // Formata a data para o formato brasileiro
-      return `
-        <div class="col-12  headline ">
+      // Função para criar HTML para uma notícia
+      function createArticleHTML(article) {
+        const date = new Date(article.publishedAt).toLocaleDateString('pt-BR');
+        return `
+       <div class="col-12  headline ">
           <div>
             <div class="autor ">
               <p>${article.author}</p> <!-- Nome da fonte -->
@@ -26,12 +27,17 @@ fetch(url)
             </h2>
           </div>
         </div>
-      `;
-    }
+        `;
+      }
 
-    // Adiciona o HTML ao contêiner
-    articles.forEach(article => {
-      container.innerHTML += createArticleHTML(article);
-    });
-  })
-  .catch(error => console.error('Error:', error));
+      // Adiciona o HTML ao contêiner específico da categoria
+      articles.forEach(article => {
+        container.innerHTML += createArticleHTML(article);
+      });
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+// Chamar a função para cada categoria
+const categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+categories.forEach(category => fetchNewsByCategory(category));
